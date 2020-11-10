90a63c8a81f39c8b16a6fdb8ab55c8b33f445135
bbzitaaaeasy <jaracenaquidel@gmail.com>
2020-11-10 10:37:44 -0300

    finalcreo

--- Taller Laboratorio01/carrito/src/App.java
+++ Taller Laboratorio01/carrito/src/App.java
@@ -1,15 +1,49 @@
+import java.lang.Math;
+import java.util.*;
+
+
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    static Random rnum = new Random();
+    public static void main(String[] args) {
+        int[] carro = new int[rnum.nextInt(20)+1];
+        addprod(carro);
+        showcar(carro);
+        detailprod(carro);
+        total(carro);
+    }
+
+    public static int[] addprod(int[] carro){
+        for(int i=0;i<carro.length;i++){
+            carro[i]=rnum.nextInt(16);
+        }
+        return carro;
+    }
+    public static  void showcar(int[] carro){
+        int productos = 0;
+        for (int i=0;i<carro.length;i++) {
+            productos += carro[i];
+        }
+        System.out.println("\ncarro contiene "+productos+" productos");
+    }
+    public static  int total(int[] carro){
+        int total = 0;
+        for (int i=0;i<carro.length;i++) {
+            total += (500+(150*i))*carro[i];
+        }
+        return total;
+    }
+    public static  void detailprod(int[] carro){
+        int subTotal = 0;
+        int total = total(carro);
+        System.out.println("Detalle compra:");
+        for(int i=0;i<carro.length;i++){
+            subTotal = (500+(150*i))*carro[i];
+            System.out.println("Cantidad producto "+(i+1)+": "+carro[i]+", subtotal: $"+subTotal);
+        }
+        System.out.println("Total de compra : $"+total);
     }
 }
 
-class Productos {
-    int array[];
-    array = new int[10];
 
 
-    for(int x = 0;x<matriz.length;x++){
-        array[x] = (int) (Math.random()*9+1);
-    }
-}
