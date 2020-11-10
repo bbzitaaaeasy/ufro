62cf73071e59b7d0e79c3e12fd8f3d8fdc48d508
bbzitaaaeasy <jaracenaquidel@gmail.com>
2020-11-10 09:47:43 -0300

    first

---
+++ Taller Laboratorio01/carrito/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+
+class Productos {
+    int array[];
+    array = new int[10];
+
+
+    for(int x = 0;x<matriz.length;x++){
+        array[x] = (int) (Math.random()*9+1);
+    }
+}
